4bafb8f6175eca30184b0d3a5f90b373abcbb437
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-05-11 11:42:44 -0400

    Added cache logic to retrieve old state immediatley, updating when XHR respondswith fresh data from API

--- scripts/app.js
+++ scripts/app.js
@@ -168,7 +168,26 @@
     var statement = 'select * from weather.forecast where woeid=' + key;
     var url = 'https://query.yahooapis.com/v1/public/yql?format=json&q=' +
         statement;
-    // TODO add cache logic here
+    
+    // Cache log
+    if ('caches' in window) {
+      /*
+       * Check if the service worker has already cached this city's weather
+       * data. If the service worker has the data, then display the cached
+       * data while the app fetches the latest data.
+       */
+      caches.match(url).then(function(response) {
+        if (response) {
+          response.json().then(function updateFromCache(json) {
+            var results = json.query.results;
+            results.key = key;
+            results.label = label;
+            results.created = json.query.created;
+            app.updateForecastCard(results);
+          });
+        }
+      });
+    }
 
     // Fetch the latest data.
     var request = new XMLHttpRequest();
